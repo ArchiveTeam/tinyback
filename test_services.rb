08a9bee5c91ff61518582a3d1da5c2dab118197b
David Triendl <david@triendl.name>
2010-12-05 05:34:06 +0100

    Add test_services.rb

---
+++ test_services.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+$: << "lib"
+
+require "tinyback/services"
+require "thread"
+
+Thread.abort_on_exception = true
+
+threads = []
+
+ObjectSpace.each_object(Class) do |obj|
+    next unless obj < TinyBack::Services::Base
+    threads << Thread.new(obj) do |klass|
+        Thread.current[:name] = klass.to_s.split("::").last
+        Thread.current[:status] = "ok"
+        service = klass.new
+        begin
+            service.fetch "0"
+        rescue TinyBack::Services::NoRedirectError
+        rescue TinyBack::Services::ServiceBlockedError
+            Thread.current[:status] = "blocked"
+        rescue => e
+            Thread.current[:status] = "error (#{e.class})"
+        end
+    end
+end
+
+threads.each do |thread|
+    thread.join
+    puts "#{thread[:name]}: #{thread[:status]}"
+end
